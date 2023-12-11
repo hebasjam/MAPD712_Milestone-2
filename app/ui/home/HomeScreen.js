ba1d64546c67344192a615f200b689efcc761979
MAC <Hebas@loni.ai>
2023-12-11 15:37:11 +0300

    bug fix

--- app/ui/home/HomeScreen.js
+++ app/ui/home/HomeScreen.js
@@ -80,7 +80,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     margin: 8,
     backgroundColor: 'white',
-    alignItems: 'left',
+    alignItems: 'center',
     width: '100%',
     height: 70,
     textAlign: 'center',
